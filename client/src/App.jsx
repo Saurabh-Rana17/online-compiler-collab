edeea07338cf295ad70f51d47e842808c56ee822
Saurabh-Rana17 <saurabhrana200317@gmail.com>
2023-09-10 20:21:04 +0530

    completed output with a box

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,17 @@ import stubs from "./stubs";
 import React, { useState, useEffect } from "react";
 import moment from "moment";
 import MonacoEditor from 'react-monaco-editor';
+import Button from '@mui/material/Button'; // Import Material-UI Button
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import Box from '@mui/material/Box'; // Import Material-UI's Box component
+
+
+
+
+
 
 
 function App() {
@@ -108,32 +119,68 @@ function App() {
     selectOnLineNumbers: true,
     fontSize: 14,
     automaticLayout: true,
+    lineNumbers: 'on', // 'off' | 'on' | 'relative'
+  glyphMargin: true,
   };
-
+  const handleChange = (event) => {
+    const shouldSwitch = window.confirm(
+      'Are you sure you want to change language? WARNING: Your current code will be lost.'
+    );
+    if (shouldSwitch) {
+      setLanguage(event.target.value);
+    }
+  };
+  const customStyles = {
+    // Change the color of the components as desired
+    backgroundColor: 'white', // Background color of FormControl
+    color: 'red', // Text color of FormControl and InputLabel
+    '& .MuiInputBase-root': {
+      color: 'green', // Text color of the selected value in Select
+    },
+  };
+  const handleClearClick = () => {
+    // Clear the editor's content
+    setCode('');
+  };  
+  const buttonStyles = {
+    submitButton: {
+      backgroundColor: 'green',
+      color: 'white',
+      marginRight: '10px',
+      '&:hover': {
+        backgroundColor: 'darkgreen', // Change the color on hover
+      },
+    },
+    clearButton: {
+      backgroundColor: 'red',
+      color: 'white',
+      '&:hover': {
+        backgroundColor: 'darkred', // Change the color on hover
+      },
+    },
+  };
   return (
     <div className="App">
       <h1>Online Code Compiler</h1>
       <div>
-        <label>Language:</label>
-        <select
-          value={language}
-          onChange={(e) => {
-            const shouldSwitch = window.confirm(
-              "Are you sure you want to change language? WARNING: Your current code will be lost."
-            );
-            if (shouldSwitch) {
-              setLanguage(e.target.value);
-            }
-          }}
-        >
-          <option value="cpp">C++</option>
-          <option value="py">Python</option>
-        </select>
+      <FormControl variant="filled" style={customStyles}>
+      <InputLabel id="language-label">Language</InputLabel>
+      <Select
+        labelId="language-label"
+        value={language}
+        onChange={handleChange}
+        label="Language"
+      >
+        <MenuItem value="cpp">C++</MenuItem>
+        <MenuItem value="py">Python</MenuItem>
+      </Select>
+    </FormControl>
       </div>
       <br />
       <div>
-        <button onClick={setDefaultLanguage}>Set Default</button>
-      </div>
+      <Button className="btn" variant="contained" >
+        Set Default
+      </Button>      </div>
       <br />
       {/* <textarea className="inp-box"
         rows="20"
@@ -153,11 +200,33 @@ function App() {
       onChange={handleEditorChange}
     />
       <br />
-      <button onClick={handleSubmit}>Submit</button>
+      
+      <div>
+      <Button className="btn"
+        variant="contained"
+        color="primary"
+        onClick={handleSubmit}
+        style={{ backgroundColor: 'green', color: 'white', marginRight: '10px' }}
+      >
+        Submit
+      </Button>
+      <Button className="btn "
+        variant="contained"
+        onClick={handleClearClick}
+        style={{ backgroundColor: 'red', color: 'white' }}
+
+        
+      >
+        Clear
+      </Button>
+    </div>
+    
+    <Box border={1} padding={2} borderColor="grey.300" borderRadius={4}>
       <p>{status}</p>
       <p>{jobId ? `Job ID: ${jobId}` : ""}</p>
       <p>{renderTimeDetails()}</p>
       <p>{output}</p>
+    </Box>
     </div>
   );
 }
