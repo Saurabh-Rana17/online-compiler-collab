4dbc71f9318c91f154a82b0cb236ed8f742a5355
Saurabh-Rana17 <saurabhrana200317@gmail.com>
2023-10-18 00:59:03 +0530

    routes added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,18 @@
 import "./App.css";
 import React from "react";
 import OfflineEditor from "./OfflineEditor";
+import Home from "./Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <OfflineEditor />
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />}></Route>
+          <Route path="/editor" element={<OfflineEditor />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ client/src/Home.jsx
@@ -0,0 +1,14 @@
+import { Button } from "@mui/material";
+
+const Home = () => {
+  return (
+    <>
+      <h1>Hello world</h1>
+      <Button>
+        {" "}
+        <a href="/editor">visit</a>
+      </Button>
+    </>
+  );
+};
+export default Home;
